5e0dc336a66929684ebdcb18a7a6a79918ba3a9c
prolayp7 <prolaypanda7@gmail.com>
2026-06-08 21:16:37 +0530

    Sync admin codebase from lcommerce admin (2026-06-08 21:16:37)

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -110,7 +110,7 @@ class OrderController extends Controller
         $this->authorize('viewAny', SellerOrder::class);
         $columns = $this->buildOrderColumns();
         return view($this->panelView('orders.index'), array_merge(compact('columns'), [
-            'defaultStatus' => OrderStatusEnum::DELIVERED(),
+            'defaultStatus' => OrderStatusEnum::ORDER_DISPATCHED(),
             'pageTitle'     => 'Order Dispatched',
             'subPage'       => 'dispatched_orders',
         ]));
